b16dcac395e117a4ffef19f9b3bdc00d66baf99a
sreeja99 <srijagodishala@gmail.com>
2020-09-29 12:18:17 +0530

    Modified:Add Multiple Person To AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -106,7 +106,7 @@ public class addressBook {
 	public void delContact(String firstName, String lastName, String address, String city, String state, int zip,
 			long phoneNumber, String email) {
 			for(addressBook obj:contactList) {
-				if(obj.getFirstName().equals(firstName)&&obj.getLastName().equals(lastName)) {
+				if(obj.getFirstName().equals(firstName&&obj.getLastName().equals(lastName)) {
 					contactList.remove(obj);
 					System.out.println("Contact deleted successfully");
 					break;
@@ -123,48 +123,98 @@ public class addressBook {
 
 
      public static void main(String[] args) {
-//    	 System.out.println("Welcome to Address Book Program");
-        Contacts person = new Contacts();
- 		int option= 1;
- 		System.out.println("Welcome to Address Book Program");
- 		Scanner sc = new Scanner(System.in);
- 		
- 		while(option>0 && option<2) {
- 			System.out.println("Enter your option\n1. Add Contact\n2. Exit");
- 			option = sc.nextInt();
- 			if(option==1) {
- 				System.out.println("Enter the details to add contact");
- 				System.out.println("Enter First Name");
- 				sc.nextLine();
- 				String firstName = sc.nextLine();
- 				System.out.println("Enter Last Name");
- 				String lastName = sc.nextLine();
- 				System.out.println("Enter Address");
- 				String  address= sc.nextLine();
- 				System.out.println("Enter City");
- 				String city = sc.nextLine();
- 				System.out.println("Enter State");
- 				String  state= sc.nextLine();
- 				System.out.println("Enter Zip");
- 				int  zip= sc.nextInt();
- 				System.out.println("Enter Phone Number");
- 				long phoneNumber = sc.nextLong();
- 				System.out.println("Enter email");
- 				sc.nextLine();
- 				String email = sc.nextLine();
- 				addressBook obj = new addressBook(firstName, lastName, address, city, state, zip, phoneNumber, email);
-			        person.addContact(obj);
- 				person.editContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-				person.delContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
- 			}
- 		}
- 		sc.close();
-
- 	}
-
- 
+System.out.println("Welcome to Address Book Program");
+		Contacts person = new Contacts();
+		int option= 1;
+		System.out.println("Welcome to Address Book Program");
+		Scanner sc = new Scanner(System.in);
 		
+		while(option>0 && option<4) {
+			AddressBookDictionary DictObj = new AddressBookDictionary();
+			System.out.println("Enter your option\n1. Add Contact\n2.Edit Contact\n3.Delete Contact\n4. Exit");
+			option = sc.nextInt();
+			//to add a contact
+			System.out.println("Enter the Address Book Name: ");
+			String addressBookName=sc.next();
+			if(option==1) {
+				System.out.println("Enter the details to add contact");
+				System.out.println("Enter First Name");
+				sc.nextLine();
+				String firstName = sc.nextLine();
+				System.out.println("Enter Last Name");
+				String lastName = sc.nextLine();
+				System.out.println("Enter Address");
+				String  address= sc.nextLine();
+				System.out.println("Enter City");
+				String city = sc.nextLine();
+				System.out.println("Enter State");
+				String  state= sc.nextLine();
+				System.out.println("Enter Zip");
+				int  zip= sc.nextInt();
+				System.out.println("Enter Phone Number");
+				long phoneNumber = sc.nextLong();
+				System.out.println("Enter email");
+				sc.nextLine();
+				String email = sc.nextLine();
+				AddressBook obj = new AddressBook(firstName, lastName, address, city, state, zip, phoneNumber, email);
+				person.addContact(obj);
+				DictObj.addAddressBook(addressBookName,obj);
+			}
+			//to edit a contact
+			else if(option ==2) {
+				System.out.println("Enter the details to edit the contact");
+				System.out.println("Enter the first name");
+				sc.nextLine();
+				String firstName = sc.nextLine();
+				System.out.println("Enter Last Name");
+				String lastName = sc.nextLine();
+				System.out.println("Enter Address");
+				String  address= sc.nextLine();
+				System.out.println("Enter City");
+				String city = sc.nextLine();
+				System.out.println("Enter State");
+				String  state= sc.nextLine();
+				System.out.println("Enter Zip");
+				int  zip= sc.nextInt();
+				System.out.println("Enter Phone Number");
+				long phoneNumber = sc.nextLong();
+				System.out.println("Enter email");
+				sc.nextLine();
+				String email = sc.nextLine();
+				person.editContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+			}
+			//to delete a contact
+			else if(option==3) {
+				System.out.println("Enter the details to delete the contact");
+				System.out.println("Enter the first name");
+				sc.nextLine();
+				String firstName = sc.nextLine();
+				System.out.println("Enter Last Name");
+				String lastName = sc.nextLine();
+				System.out.println("Enter Address");
+				String  address= sc.nextLine();
+				System.out.println("Enter City");
+				String city = sc.nextLine();
+				System.out.println("Enter State");
+				String  state= sc.nextLine();
+				System.out.println("Enter Zip");
+				int  zip= sc.nextInt();
+				System.out.println("Enter Phone Number");
+				long phoneNumber = sc.nextLong();
+				System.out.println("Enter email");
+				sc.nextLine();
+				String email = sc.nextLine();
+				person.delContact(firstName,lastName,address,city,state,zip,phoneNumber,email);
+				
+				
+			}
+		}
+		sc.close();
 
 	}
 
-	
+
+
+	}
+
+
